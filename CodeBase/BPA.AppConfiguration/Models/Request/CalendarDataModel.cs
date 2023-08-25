c9925bd3ce990de0d8eef1276c3e24b734cba3de
vkpatilhsit <vkpatil.hsit@gmail.com>
2023-08-25 11:07:38 +0530

    Configuration EXL API

---
+++ CodeBase/BPA.AppConfiguration/Models/Request/CalendarDataModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BPA.AppConfiguration.Models.Request
+{
+    public class CalendarDataModel
+    {
+        public int CalID { get; set; }
+
+        public int CalendarID { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string MonthYear { get; set; }
+        public string CalendarName { get; set; }
+        public string WeekStartDay
+        {
+            get;
+            set;
+        }
+        public string StartDateofMonth
+        {
+            get;
+            set;
+        }
+        public string EndDateofMonth
+        {
+            get;
+            set;
+        }
+        public bool Disabled
+        {
+            get;
+            set;
+        }
+        public List<Calendarweek> WeekList { get; set; }
+    }
+
+
+    [Serializable]
+    public class Calendarweek
+    {
+        public Calendarweek()
+        { }
+
+        public Nullable<int> miWeek { get; set; }
+
+        [DataType(DataType.Date)] // making data type as date     
+        public DateTime DisplayEndDate { get; set; }
+
+        [DataType(DataType.Date)] // making data type as date   
+        public DateTime DisplayStartDate { get; set; }
+
+        public string msRowState { get; set; }
+
+        public Nullable<int> miCalendarId { get; set; }
+
+        //  public IEnumerable<object> data { get; set; }
+    }
+}
---
+++ CodeBase/BPA.AppConfiguration/Models/Request/MasterValueModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BPA.AppConfiguration.Models.Request
+{
+    public class MasterValueModel
+    {
+        public MasterValueModel()
+        {
+            ValueList = new();
+        }
+        public int MasterValueID
+        {
+            get;
+            set;
+        }
+        public string MasterType
+        {
+            get;
+            set;
+        }
+        public bool? Disabled
+        {
+            get;
+            set;
+        }
+
+
+        public List<MasterValueList> ValueList { get; set; }
+        public bool ShouldSerializeValueList()
+        {
+            if (ValueList.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+    public class MasterValueList
+    {
+        public int FieldID
+        {
+            get;
+            set;
+        }
+        public string Values
+        {
+            get;
+            set;
+        }
+        public bool? Disabled
+        {
+            get;
+            set;
+        }
+    }
+}
